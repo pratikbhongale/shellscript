81f28e3270bdf0792ce166ad8f27837a1b7e29d4
pratikbhongale <pratikbhongale01@gmail.com>
2026-01-04 12:24:27 +0530

    commit on 4th jan

--- loop_2.sh
+++ loop_2.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+<<comment
 users="Dev QA Prod"
 for user in $users
 do
-echo -n $user
+echo $user
+done
+
+for name in Alice Bob Charlie
+do
+echo "Hello $name"
+done
+
+#loop with numbers
+for num in 1 2 3 4 5 6
+do
+echo "Number is $num"
+done
+comment
+#loops with numbers using curly braces
+#for num in {1..5}
+#do
+#echo "Num is $num"
+#done
+for file in *
+do
+#clear
+echo "File name is: $file"
+sleep 1
 done
 
